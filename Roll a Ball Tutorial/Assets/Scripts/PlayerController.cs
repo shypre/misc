00edfc535c8ec5fbc3671423569bfae8f2d924bd
shypre <shyprea2@gmail.com>
2016-11-01 08:28:17 -0700

    stuff

---
+++ Roll a Ball Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+	void Update()
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Verical");
+
+       		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement);
+	}
+}
