b1d2c97205ffede7f424b731e1c7ddf6bf0207e5
shypre <shyprea2@gmail.com>
2017-01-20 18:48:48 -0800

    stuff~~~

--- 18 Wheels of Liberty/18 Wheels of Liberty/Assets/Scripts/Movement.cs
+++ 18 Wheels of Liberty/18 Wheels of Liberty/Assets/Scripts/Movement.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
 	public Rigidbody rbCharacter;
-	public float MoveSpeedMultiplier, RotateSpeedMultiplier;
+	public float MoveSpeedMultiplier, RotateSpeedMultiplier, VelocityGainMultiplier;
 	private float Velocity;
-	private float HorizontalAxisValue, VerticalAxisValue;
-    private Vector3 Rotation;
+	private bool isForward, isBack, isLeft, isRight;
+    private Vector3 Rotation, Movement;
+    public Text Speed;
 
 	void Start()
 	{
@@ -17,32 +19,60 @@ public class Movement : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		HorizontalAxisValue = Input.GetAxis("Horizontal");
-		VerticalAxisValue = Input.GetAxis("Vertical");
-        if (Velocity >= 0)
+        isForward = Input.GetButton("Forward");
+        isBack = Input.GetButton("Back");
+        isLeft = Input.GetButton("Left");
+        isRight = Input.GetButton("Right");
+        if (Velocity == 0.0f)
         {
-            if (VerticalAxisValue > 0)
+            if (isBack)
+            {
+                Velocity = -0.01f;
+            }
+            else if (isForward)
             {
                 Velocity += MoveSpeedMultiplier;
             }
-            else if (VerticalAxisValue < 0)
-            {
-                Velocity -= MoveSpeedMultiplier;
-            }
         }
-        else if (Velocity < 0)
+		else if (Velocity > 0.0f)
         {
-            if (VerticalAxisValue >= 0)
+			if (isForward)
             {
-                Velocity = 0;
-            }
+                Velocity += (10 - Velocity)/(Velocity * MoveSpeedMultiplier + 10000);
+			}
+			else if (isBack)
+            {
+                Velocity -= (MoveSpeedMultiplier * 0.1f) + (Velocity * 0.01f) + 0.001f;
+			}
             else
             {
-                Velocity = Velocity * 1.05;
+                Velocity -= (Velocity * 0.01f) + 0.001f;
             }
+		}
+        else if (Velocity < 0.0f)
+        {
+            if (!(isBack))
+            {
+				Velocity = 0.0f;
+			}
+            else if (isBack)
+            {
+                Velocity = Velocity * VelocityGainMultiplier;
+			}
+		}
+        if (isLeft)
+        {
+            Rotation = new Vector3(0, RotateSpeedMultiplier * -1, 0);
+            rbCharacter.transform.Rotate (Rotation);
         }
-        Rotation = new Vector3(0, 0, HorizontalAxisValue);
-        rbCharacter.transform.Rotate(Rotation);
+        else if (isRight)
+        {
+            Rotation = new Vector3(0, RotateSpeedMultiplier, 0);
+            rbCharacter.transform.Rotate (Rotation);
+        }
+		Movement = new Vector3 (0, 0, Velocity);
+		rbCharacter.transform.Translate (Movement);
 
+        Speed.text = "Speed: " + Velocity.ToString();
 	}
 }
